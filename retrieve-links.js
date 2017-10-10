b06afb01d57ca3897f36aa2b69f1a5dbccfd29ab
Romario Maxwell <romario.maxwell@gmail.com>
2017-10-10 14:07:11 -0500

    update HTML parser
    * corrected an issue with parsing links with leading whitespace by removing it before trying to parse
    * now parses all pages containing links instead of just the first one only
    
    N.B. Synchronous HMLHttpRequest's are being used but this is deprecated so this will be corrected in a subsequent commit.

--- retrieve-links.js
+++ retrieve-links.js
@@ -1,59 +1,74 @@
 (function(window, document, undefined) {
   'use strict';
 
-  const httpRequest = new XMLHttpRequest();
-  httpRequest.onreadystatechange = function() {
-    if (httpRequest.readyState === XMLHttpRequest.DONE &&
-          httpRequest.status === 200) {
-      const parser = new DOMParser();
+  const parser = new DOMParser();
 
-      const doc =
-        parser.parseFromString(httpRequest.responseText, 'text/html');
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+      let doc = parser.parseFromString(xhr.responseText, 'text/html');
+      const pages = doc.querySelector('.pages').innerText;
+      const res = pages.match(/Page \d+ of (\d+)/);
+      const npages = parseInt(res[1], 10);
 
-      // const pages = doc.querySelector('.pages').innerText;
+      const re = /(?:wwww*\d*\.)?(.+\.gov\.jm)/;
+      let csv = 'Domain Name,Agency\n';
 
-      // const res = pages.match(/Page \d+ of (\d+)/);
-      // const npages = parseInt(res[1], 10);
+      for (let i = 1; i <= npages; i++) {
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            doc = parser.parseFromString(xhr.responseText, 'text/html');
 
-      const links = doc.getElementById('links').getElementsByTagName('li');
+            let links =
+                doc.getElementById('links').getElementsByTagName('li');
 
-      const re = /(?:www\.)?(.+\.gov\.jm)/;
+            let len = links.length;
+            for (let j = 0; j < len; j++) {
+              let url = links[j].getElementsByTagName('span')[0].innerText;
 
-      let csv = "";
+              url = url.trim();
 
-      const len = links.length;
-      for (let i = 0; i < len; i++) {
-        let url = links[i].getElementsByTagName('span')[0].innerText;
+              if (!url.startsWith('http://') && !url.startsWith('https://')) {
+                url = 'http://' + url;
+              }
 
-        if (!url.startsWith('http://') && !url.startsWith('https://')) {
-          url = 'http://' + url;
-        }
+              let parsedUrl = null;
+              try {
+                parsedUrl = new URL(url);
+              } catch (e) {
+                console.log('' + e);
+                continue;
+              }
 
-        let parsedUrl = null;
-        try {
-          parsedUrl = new URL(url);
-        } catch (e) {
-          console.log('' + e);
-          continue;
-        }
+              let hostname = parsedUrl.hostname;
 
-        let hostname = parsedUrl.hostname;
+              let arr = hostname.match(re);
+              if (arr === null) {
+                console.log('not a .gov.jm hostname: ' + hostname);
+                continue;
+              }
 
-        let arr = hostname.match(re);
-        if (arr === null) {
-          console.log('not a .gov.jm hostname: ' + hostname);
-          continue;
-        }
+              let domainName = arr[1];
 
-        let domainName = arr[1];
+              let agency =
+                  links[j].getElementsByTagName('strong')[0].innerText;
 
-        let agency = links[i].getElementsByTagName('strong')[0].innerText;
-        csv += domainName + ',' + agency + '\n';
+              let pos = agency.indexOf(',');
+              if (pos !== -1) {
+                agency = '"' + agency + '"';
+              }
+
+              csv += domainName + ',' + agency + '\n';
+            }
+          }
+        };
+        xhr.open('GET', '/government/links/page/' + i, false);
+        xhr.send();
       }
 
       console.log(csv);
     }
   };
-  httpRequest.open('GET', '/government/links/', true);
-  httpRequest.send();
+  xhr.open('GET', '/government/links/', true);
+  xhr.send();
 })(window, document);
